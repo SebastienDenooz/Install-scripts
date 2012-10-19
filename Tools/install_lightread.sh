6759e277d15fe40b040a34001fa1ed0b10b605e3
Sébastien Denooz <sde@voxteneo.com>
2012-10-19 11:03:59 +0200

    Add a light rss reader and a qrcode reader

---
+++ Tools/install_lightread.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:cooperjona/lightread
+sudo apt-get update
+sudo apt-get install lightread
---
+++ Tools/install_webbar.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:lukasrms/webbar
+sudo apt-get update
+sudo apt-get install webbar
