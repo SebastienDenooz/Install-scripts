e84d8c0b1ea0b3bb847c9615a224f9581d5a9644
Sébastien Denooz <sebastien.denooz@lattirail.net>
2012-11-01 01:07:01 +0100

    Install the good deb

--- Tools/install_xmind.sh
+++ Tools/install_xmind.sh
@@ -9,7 +9,7 @@ then
 	rm xmind-linux-3.3.0.201208102038_amd64.deb
 else 
 	wget http://www.xmind.net/xmind/downloads/xmind-linux-3.3.0.201208102038_i386.deb
-	sudo dpkg -i xmind-linux-3.3.0.201208102038_amd64.deb
+	sudo dpkg -i xmind-linux-3.3.0.201208102038_i386.deb
 	rm xmind-linux-3.3.0.201208102038_i386.deb
 fi
 
