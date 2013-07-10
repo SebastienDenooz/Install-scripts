85bed3e468911638414392e6ef8e5d679318e2de
Sébastien Denooz <sde@voxteneo.com>
2013-07-10 17:06:59 +0200

    Debug & cleaning

--- video_to_mp3.py
+++ video_to_mp3.py
@@ -1,20 +1,19 @@
 import os
-from subprocess import call, Popen
+from subprocess import call
 
 for files in os.listdir("."):
-    if files.endswith(".webm"):
-        if not os.path.isfile(files.replace(".webm",".mp3")):
-            print "Processing {0} ...".format(files)
-            i = 0
-            while os.path.isfile(files.replace(".webm",".temp{0}.ogg".format(i))):
-                i += 1
-            audio_extract = call(["avconv",
-            	"-i","{0}/{1}".format(os.getcwd(),files),
-            	"-vn",
-            	"-ar","44100",
-            	"-ac","2",
-            	"-ab", "320",
-            	"-metadata",'title={0}'.format(files.replace(".webm","")),
-            	"-acodec", "libmp3lame",
-            	"{0}/{1}".format(os.getcwd(),files.replace(".webm",".mp3".format(i)))
-            	])
+    if files.endswith(".webm") and not os.path.isfile(files.replace(".webm",".mp3")):
+        print "Processing {0} ...".format(files)
+        i = 0
+        while os.path.isfile(files.replace(".webm",".temp{0}.ogg".format(i))):
+            i += 1
+        call(["avconv",
+        	"-i","{0}/{1}".format(os.getcwd(),files),
+        	"-vn",
+        	"-ar","44100",
+        	"-ac","2",
+        	"-ab", "320000",
+        	"-metadata",'title={0}'.format(files.replace(".webm","")),
+        	"-acodec", "libmp3lame",
+        	"{0}/{1}".format(os.getcwd(),files.replace(".webm",".mp3".format(i)))
+        	])
